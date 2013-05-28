cd80541c12d374a2a05fbd00e243ebc9b95acc26
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2013-05-28 16:53:03 +0200

    Style: logoslide portlet

--- src/libertic.event/src/libertic/event/static/js/theme.js
+++ src/libertic.event/src/libertic/event/static/js/theme.js
@@ -1,5 +1,12 @@
 
 function initializeliberticevent(){
 
+    //  CarouFredsel with several elements
+    $('#operators-list').carouFredSel({
+        auto: false,
+        prev: '#prev1',
+        next: '#next1',
+        });
+    $('#suppliers-list').carouFredSel();
 }
 jQuery(initializeliberticevent);
